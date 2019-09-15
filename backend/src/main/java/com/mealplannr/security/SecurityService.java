1cc0f495162e52cb74e74ab9ebfac51168adbe15
Stuart Leyland-Cole <stuartleyland@gmail.com>
2019-09-15 16:47:19 +0100

    Add a stack for user contexts

--- backend/src/main/java/com/mealplannr/security/SecurityService.java
+++ backend/src/main/java/com/mealplannr/security/SecurityService.java
@@ -1,5 +1,8 @@
 package com.mealplannr.security;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -7,26 +10,61 @@ import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.subject.support.SubjectThreadState;
+import org.apache.shiro.util.ThreadState;
 
 /**
- * Sets up and interacts with Apache Shiro which is the base library performing authorisation capability
+ * Sets up and interacts with Apache Shiro which is the base library providing authorisation capability
  */
 @Singleton
 public class SecurityService {
 
     private final SecurityManager securityManager;
+    private Deque<UserContext> authStack = new ArrayDeque<>();
 
     @Inject
     SecurityService(final SecurityManager securityManager) {
         this.securityManager = securityManager;
     }
 
-    public SubjectThreadState initialiseSubject(final String userId) {
+    public ThreadState initialiseContext(final String userId) {
+        authStack = new ArrayDeque<>();
+        return pushUser(userId);
+    }
+
+    public ThreadState pushUser(final String userId) {
+        authStack.push(new UserContext(userId));
+        return createThreadState(userId);
+    }
+
+    private ThreadState createThreadState(final String userId) {
         final Subject subject = new Subject.Builder(securityManager)
                 .principals(new SimplePrincipalCollection(userId, DynamoDbRealm.NAME))
                 .buildSubject();
         final SubjectThreadState subjectThreadState = new SubjectThreadState(subject);
         subjectThreadState.bind();
         return subjectThreadState;
+    }
+
+    public ThreadState popUser() {
+        if (!authStack.isEmpty()) {
+            authStack.pop();
+            final UserContext previousUserContext = authStack.peek();
+            if (previousUserContext != null) {
+                return pushUser(previousUserContext.userId);
+            }
+        }
+        return null;
+    }
+
+    public boolean authStackIsEmpty() {
+        return authStack.isEmpty();
+    }
+
+    private class UserContext {
+        private final String userId;
+
+        private UserContext(final String userId) {
+            this.userId = userId;
+        }
     }
 }
--- backend/src/test/java/com/mealplannr/security/SecurityServiceLT.java
+++ backend/src/test/java/com/mealplannr/security/SecurityServiceLT.java
@@ -5,14 +5,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import javax.inject.Inject;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.support.SubjectThreadState;
+import org.apache.shiro.util.ThreadState;
 import org.junit.jupiter.api.Test;
 
 import com.mealplannr.test.LocalTestBase;
 
 public class SecurityServiceLT extends LocalTestBase {
 
-    private static final String USER_ID = "user1";
+    private static final String USER_1_ID = "user1";
+    private static final String USER_2_ID = "user2";
     @Inject
     SecurityService securityService;
 
@@ -23,22 +24,55 @@ public class SecurityServiceLT extends LocalTestBase {
 
     @Test
     void security_context_is_initialised() {
-        final SubjectThreadState subjectThreadState = securityService.initialiseSubject(USER_ID);
+        final ThreadState subjectThreadState = securityService.initialiseContext(USER_1_ID);
 
         assertThat(subjectThreadState).isNotNull();
     }
 
     @Test
     void subject_is_set() {
-        securityService.initialiseSubject(USER_ID);
+        securityService.initialiseContext(USER_1_ID);
 
         assertThat(SecurityUtils.getSubject()).isNotNull();
     }
 
     @Test
     void user_id_is_set_as_subject_principle() {
-        securityService.initialiseSubject(USER_ID);
+        securityService.initialiseContext(USER_1_ID);
 
-        assertThat(SecurityUtils.getSubject().getPrincipal()).isEqualTo(USER_ID);
+        assertThat(SecurityUtils.getSubject().getPrincipal()).isEqualTo(USER_1_ID);
+    }
+
+    @Test
+    void users_teams_are_set_as_session_attributes() {
+
+    }
+
+    @Test
+    void initialiseContext_resets_auth_stack() {
+        securityService.initialiseContext(USER_1_ID);
+
+        securityService.popUser();
+
+        assertThat(securityService.authStackIsEmpty()).isTrue();
+    }
+
+    @Test
+    void can_change_subject() {
+        securityService.initialiseContext(USER_1_ID);
+
+        securityService.pushUser(USER_2_ID);
+
+        assertThat(SecurityUtils.getSubject().getPrincipal()).isEqualTo(USER_2_ID);
+    }
+
+    @Test
+    void can_change_subject_and_back_to_original() {
+        securityService.initialiseContext(USER_1_ID);
+        securityService.pushUser(USER_2_ID);
+
+        securityService.popUser();
+
+        assertThat(SecurityUtils.getSubject().getPrincipal()).isEqualTo(USER_1_ID);
     }
 }
